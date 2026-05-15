fd70ec001a632dbe499448cf1986455a89fd5662
zzing <wkdwldms2001@naver.com>
2026-05-15 17:19:13 +0900

    feat: 로그인 화면 ui 수정 완료 #86

--- app/src/main/java/com/bbip/bbipit/presentation/auth/ui/SignInScreen.kt
+++ app/src/main/java/com/bbip/bbipit/presentation/auth/ui/SignInScreen.kt
@@ -1,12 +1,13 @@
 package com.bbip.bbipit.presentation.auth.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,8 +20,6 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -28,6 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,98 +35,113 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bbip.bbipit.R
+import com.bbip.bbipit.core.navigation.Routes
 import com.bbip.bbipit.core.ui.theme.Typography
-import com.bbip.bbipit.core.ui.theme.fontDefault
+import com.bbip.bbipit.core.ui.theme.background
 import com.bbip.bbipit.core.ui.theme.primary
-import com.bbip.bbipit.presentation.base.BackgroundBox
+import com.bbip.bbipit.presentation.auth.viewmodel.SignInEvent
+import com.bbip.bbipit.presentation.auth.viewmodel.SignInViewModel
 
 @Composable
-fun SignInScreen(navController: NavController) {
+fun SignInScreen(navController: NavController, viewModel: SignInViewModel = hiltViewModel()) {
+
+    val focusManager = LocalFocusManager.current
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
-    BackgroundBox {
-        Box(modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center) {
-            Card(modifier = Modifier.fillMaxHeight(0.8f).fillMaxWidth(0.8f),
-                shape = RoundedCornerShape(20.dp),
-                colors = CardDefaults.cardColors(containerColor = Color(0xFFEDEDED)),
-                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)) {
-                Column(modifier = Modifier.padding(10.dp).fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(17.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-
-                    Image(painter = painterResource(R.drawable.logo),
-                        contentDescription = "로고",
-                        modifier = Modifier.size(120.dp).padding(top = 20.dp)
-                    )
-
-                    Text("BBip-It", style = Typography.titleLarge)
-                    Text("심장이 반응하는 거리, 삐빗-", style = Typography.bodySmall)
-
-                    Column(modifier = Modifier.fillMaxWidth().padding(7.dp),
-                        verticalArrangement = Arrangement.spacedBy(17.dp),
-                        horizontalAlignment = Alignment.Start) {
-                        Spacer(modifier = Modifier.height(17.dp))
-                        Text("이메일", style = Typography.bodyMedium, fontWeight = FontWeight.Bold)
-                        InputField(value = email,
-                            onValueChange = {email = it},
-                            placeholder = "이메일을 입력해주세요")
-                        Text("비밀번호", style = Typography.bodyMedium, fontWeight = FontWeight.Bold)
-                        InputField(value = password,
-                            onValueChange = {password = it},
-                            placeholder = "비밀번호를 입력해주세요",
-                            isPassword = true)
-
-                        Button({},
-                            colors = ButtonDefaults.buttonColors(primary),
-                            modifier = Modifier.fillMaxWidth(),
-                            shape = RoundedCornerShape(8.dp)
-                        ) {
-                            Text("로그인", style = Typography.bodyMedium, color = Color.White, fontWeight = FontWeight.Bold)
-                        }
-
-                        Row(modifier = Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically) {
-                            HorizontalDivider(thickness = 0.7.dp, color = Color.LightGray, modifier = Modifier.padding(end = 7.dp).weight(1f))
-                            Text("또는", style = Typography.bodySmall)
-                            HorizontalDivider(thickness = 0.7.dp, color = Color.LightGray, modifier = Modifier.padding(start = 7.dp).weight(1f))
-                        }
-                        Row(modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly) {
-                            Image(painter = painterResource(R.drawable.ic_signin_google),
-                                contentDescription = "구글 소셜 로그인",
-                                modifier = Modifier.size(50.dp)
-                            )
-                            Image(painter = painterResource(R.drawable.ic_signin_kakao),
-                                contentDescription = "카카오 소셜 로그인",
-                                modifier = Modifier.size(50.dp)
-                            )
-                        }
+    LaunchedEffect(Unit) {
+        viewModel.events.collect { event ->
+            when(event){
+                is SignInEvent.NavigateToSignUp ->
+                    navController.navigate(Routes.SignUp)
+                is SignInEvent.NavigateToHome ->
+                    navController.navigate(Routes.Map){
+                        popUpTo(0){ inclusive = true }
                     }
-
-                    Row() {
-                        Text("계정이 없으신가요? ", style = Typography.bodySmall)
-                        Text("회원가입", style = Typography.bodySmall, color = fontDefault, fontWeight = FontWeight.Bold)
-                    }
-
-                }
-
             }
-
         }
     }
 
+    Column(modifier = Modifier.fillMaxSize().padding(30.dp)
+        .background(background)
+        .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null
+        ){
+            focusManager.clearFocus()
+        },
+        verticalArrangement = Arrangement.spacedBy(17.dp, alignment = Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Image(painter = painterResource(R.drawable.logo),
+            contentDescription = "로고",
+            modifier = Modifier.size(120.dp).padding(top = 20.dp)
+        )
+        Text("삐빗- 심장이 반응하는 거리", style = Typography.bodySmall)
+        Spacer(modifier = Modifier.height(17.dp))
+        Text("이메일", style = Typography.bodyMedium, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth())
+        InputField(value = email,
+            onValueChange = {email = it},
+            placeholder = "이메일을 입력해주세요")
+        Text("비밀번호", style = Typography.bodyMedium, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth())
+        InputField(value = password,
+            onValueChange = {password = it},
+            placeholder = "비밀번호를 입력해주세요",
+            isPassword = true)
+
+        Button({viewModel.signIn()},
+            colors = ButtonDefaults.buttonColors(primary),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text("로그인", style = Typography.bodyMedium, color = Color.White, fontWeight = FontWeight.Bold)
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically) {
+            HorizontalDivider(thickness = 0.7.dp, color = Color.LightGray, modifier = Modifier.padding(end = 7.dp).weight(1f))
+            Text("또는", style = Typography.bodySmall)
+            HorizontalDivider(thickness = 0.7.dp, color = Color.LightGray, modifier = Modifier.padding(start = 7.dp).weight(1f))
+        }
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Image(painter = painterResource(R.drawable.ic_signin_google),
+                contentDescription = "구글 소셜 로그인",
+                modifier = Modifier.size(50.dp)
+            )
+            Image(painter = painterResource(R.drawable.ic_signin_kakao),
+                contentDescription = "카카오 소셜 로그인",
+                modifier = Modifier.size(50.dp)
+            )
+        }
+
+        Row() {
+            Text("계정이 없으신가요? ", style = Typography.bodySmall)
+            Text("회원가입",
+                style = Typography.bodySmall,
+                color = primary,
+                fontWeight = FontWeight.Bold,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable(){viewModel.signUp()})
+        }
+    }
+
+
+
 }
+
 
 @Composable
 fun InputField(value: String, onValueChange: (String) -> Unit, placeholder: String, isPassword: Boolean = false){
