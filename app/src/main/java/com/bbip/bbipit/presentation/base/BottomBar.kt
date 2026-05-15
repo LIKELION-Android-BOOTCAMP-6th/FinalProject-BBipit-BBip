1b4104d032f043da4d0b31dc49a280d4a71f1855
zzing <wkdwldms2001@naver.com>
2026-05-15 14:46:57 +0900

    chore : 하단바 디자인 변경

--- app/src/main/java/com/bbip/bbipit/presentation/base/BottomBar.kt
+++ app/src/main/java/com/bbip/bbipit/presentation/base/BottomBar.kt
@@ -5,16 +5,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Chat
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Sms
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Group
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Sms
+import androidx.compose.material.icons.rounded.ChatBubbleOutline
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
@@ -22,12 +28,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.bbip.bbipit.core.navigation.Routes
+import com.bbip.bbipit.core.ui.theme.background
 import com.bbip.bbipit.core.ui.theme.bottomBarBack
 import com.bbip.bbipit.core.ui.theme.primary
 
@@ -39,14 +47,48 @@ fun BottomBar(navController: NavController){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .navigationBarsPadding()
-        ,
-        shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
-        color = bottomBarBack.copy(alpha = 0.4f),
-//        contentColor = contentColorFor(bottomBarBack),
+        .padding(20.dp),
+        shape = RoundedCornerShape(50.dp),
+        color = background,
+        shadowElevation = 17.dp
     ) {
         Row(modifier = Modifier.fillMaxWidth().height(68.dp),
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically) {
+
+            val isFriendSelected = currentDestination?.hasRoute<Routes.MyPage>() == true
+            IconButton(
+                onClick = {
+                    if (!isFriendSelected){
+                        navController.navigate(Routes.MyPage){
+                            popUpTo(Routes.Map){ inclusive = false}
+                            launchSingleTop = true
+                        }
+                    }
+                },
+            ) {
+                Icon(imageVector = if(isFriendSelected) Icons.Default.Group else Icons.Outlined.Group,
+                    contentDescription = "친구창",
+                    tint = if (isFriendSelected) primary else Color.Gray
+                )
+            }
+
+            val isChatSelected = currentDestination?.hasRoute<Routes.ChatList>() == true
+            IconButton(
+                onClick = {
+                    if (!isChatSelected){
+                        navController.navigate(Routes.ChatList){
+                            popUpTo(Routes.Map){ inclusive = false}
+                            launchSingleTop = true
+                        }
+                    }
+                },
+            ) {
+                Icon(imageVector = if(isChatSelected) Icons.Filled.Sms else Icons.Outlined.Sms,
+                    contentDescription = "채팅",
+                    tint = if (isChatSelected) primary else Color.Gray
+                )
+            }
 
             val isMapSelected = currentDestination?.hasRoute<Routes.Map>() == true
             IconButton(
@@ -65,22 +107,7 @@ fun BottomBar(navController: NavController){
                 )
             }
 
-            val isChatSelected = currentDestination?.hasRoute<Routes.ChatList>() == true
-            IconButton(
-                onClick = {
-                    if (!isChatSelected){
-                        navController.navigate(Routes.ChatList){
-                            popUpTo(Routes.Map){ inclusive = false}
-                            launchSingleTop = true
-                        }
-                    }
-                },
-            ) {
-                Icon(imageVector = if(isChatSelected) Icons.AutoMirrored.Filled.Chat else Icons.Default.ChatBubbleOutline,
-                    contentDescription = "채팅",
-                    tint = if (isChatSelected) primary else Color.Gray
-                )
-            }
+
 
             val isNotiSelected = currentDestination?.hasRoute<Routes.Noti>() == true
             IconButton(
@@ -114,6 +141,7 @@ fun BottomBar(navController: NavController){
                     tint = if (isMyPageSelected) primary else Color.Gray
                 )
             }
+
         }
 
 
