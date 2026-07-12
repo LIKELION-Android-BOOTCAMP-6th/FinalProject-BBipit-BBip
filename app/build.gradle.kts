5714d879b7a7bdac6c9bd0d8567d40829b2cc79a
장지은 <wkdwldms2001@naver.com>
2026-07-12 16:25:28 +0900

    release : 2.0 버전 출시

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.bbip.bbipit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.1"
+        versionCode = 6
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["MAP_KEY"] =
